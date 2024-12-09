0e88d0675427d0416a46f4159a8956071269eddc
Rithalin <kleinert.rk@gmail.com>
2024-12-09 02:24:44 +0100

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'String Art Generator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'String Art Generator Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const Center(
+        child: Text(
+          'Hello, String Art Generator!',
+        ),
+      ),
+    );
+  }
+}
